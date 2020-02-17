47c42261c04f7e5c0a29fb077e3b475be4ea2afc
Haider <haider.haider168@gmail.com>
2020-02-17 15:13:27 +0530

    failed test-case try

--- test/test-pages.js
+++ test/test-pages.js
@@ -7,5 +7,5 @@ it('this is second test-case', function() {
     expect(1+2+1).to.eq(4)
 });
 it('this is third test-case', function() {
-    expect(1+2+1+1).to.eq(5)
+    expect(1+2+1+1).to.eq(4)
 });
