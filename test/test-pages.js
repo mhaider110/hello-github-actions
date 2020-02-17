9c3d171059f4a7f95a0abdf7805e6a477405779c
Haider <haider.haider168@gmail.com>
2020-02-17 15:07:46 +0530

    updating test-cases

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,3 +3,9 @@ var expect  = require('chai').expect;
 it('Main page content', function() {
     expect(1+2).to.eq(3)
 });
+it('this is second test-case', function() {
+    expect(1+2+1).to.eq(4)
+});
+it('this is third test-case', function() {
+    expect(1+2+1+1).to.eq(5)
+});
